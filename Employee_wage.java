d1d72c91de1c1f14c43101e3b9d24d21a7cad407
swapnil yadav <swapnilmaya19compeng@student.mes.ac.in>
2024-05-08 10:32:54 +0530

    Uc1-emp_present_or_absent added

--- Employee_wage.java
+++ Employee_wage.java
@@ -3,5 +3,12 @@
 public class Employee_wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage.");
+        int Full_Time = 1;
+
+        double emp_check = Math.floor(Math.random()*10 % 2);
+        if(Full_Time == emp_check)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is not present");
     }
 }
