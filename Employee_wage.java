722a29e55f57301cd4377f695e4a847e90e21f15
swapnil yadav <swapnilmaya19compeng@student.mes.ac.in>
2024-05-08 10:44:48 +0530

    uc2_emp_daily_wage

--- Employee_wage.java
+++ Employee_wage.java
@@ -3,12 +3,25 @@
 public class Employee_wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage.");
-        int Full_Time = 1;
+        int FULL_TIME_EMP = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int EMP_WORK_HOUR = 8;
+        double daily_emp_wage =0;
 
         double emp_check = Math.floor(Math.random()*10 % 2);
-        if(Full_Time == emp_check)
-            System.out.println("Employee is present");
+        if(FULL_TIME_EMP == emp_check)
+            System.out.println("Employee are present");
         else
-            System.out.println("Employee is not present");
+            System.out.println("Employee are not present");
+
+
+        double empcheck = Math.floor(Math.random()*10%2);
+
+        if(empcheck == FULL_TIME_EMP){
+            daily_emp_wage = EMP_WORK_HOUR * EMP_RATE_PER_HOUR;
+        }
+
+        System.out.println("Employee Daily Wage is : " + daily_emp_wage);
+
     }
 }
