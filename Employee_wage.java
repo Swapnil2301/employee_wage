597bec4f5e449758a67c6174032ac4826637dcbc
swapnil yadav <swapnilmaya19compeng@student.mes.ac.in>
2024-05-08 11:03:00 +0530

    ec5_employee_monthly_wage

--- Employee_wage.java
+++ Employee_wage.java
@@ -4,35 +4,30 @@ public class Employee_wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage.");
         int FULL_TIME_EMP = 1;
-        int PART_TIME_EMP=2;
+        int PART_TIME_EMP = 2;
         int EMP_RATE_PER_HOUR = 20;
         int EMP_FULL_TIME_HOUR = 8;
-        int EMP_PART_TIME_HOUR=4;
-        double daily_emp_wage =0;
+        int EMP_PART_TIME_HOUR = 4;
+        int EMP_WORK_DAYS = 20;
+        int total_wage = 0;
+        double daily_emp_wage = 0;
 
+        for (int i = 0; i < EMP_WORK_DAYS; i++) {
+            int empcheck = (int) Math.floor(Math.random() * 10 % 3);
 
-        int empcheck = (int)Math.floor(Math.random()*10%3);
-
-        switch (empcheck) {
-            case 1:
-                daily_emp_wage = EMP_FULL_TIME_HOUR * EMP_RATE_PER_HOUR;
+            if (empcheck == FULL_TIME_EMP) {
                 System.out.println("Employee are present for full time.");
-                System.out.println("Employee Daily Wage is : " + daily_emp_wage);
-                break;
-
-            case 2:
-                daily_emp_wage = EMP_PART_TIME_HOUR * EMP_RATE_PER_HOUR;
+                daily_emp_wage = EMP_FULL_TIME_HOUR * EMP_RATE_PER_HOUR;
+            } else if (empcheck == PART_TIME_EMP) {
                 System.out.println("Employee are present for part time.");
-                System.out.println("Employee wage is : "+daily_emp_wage);
-                break;
-
-            default:
+                daily_emp_wage = EMP_PART_TIME_HOUR * EMP_RATE_PER_HOUR;
+            } else {
                 System.out.println("Employee are not present");
-                daily_emp_wage=0;
-                System.out.println("Employee Daily Wage is : " + daily_emp_wage);
-                break;
-
-
+                daily_emp_wage = 0;
+            }
+            total_wage += daily_emp_wage;
+            System.out.println("Employee Daily Wage is : " + daily_emp_wage);
+        }
+        System.out.println("The Wages for a month is :" + total_wage);
     }
-}
 }
