f70f6dcb0a2963e94f82deccd30ec72edd46b03d
swapnil yadav <swapnilmaya19compeng@student.mes.ac.in>
2024-05-16 10:03:35 +0530

    UC7 added

--- Employee_wage.java
+++ Employee_wage.java
@@ -1,45 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Employee_wage {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage.");
-        int FULL_TIME_EMP = 1;
-        int PART_TIME_EMP=2;
-        int EMP_RATE_PER_HOUR = 20;
-        int EMP_FULL_TIME_HOUR = 8;
-        int EMP_PART_TIME_HOUR=4;
-        int EMP_WORK_DAYS = 20;
-        int EMP_WORK_HOUR = 100;
+
+    int FULL_TIME_EMP = 1;
+    int PART_TIME_EMP = 2;
+    int EMP_RATE_PER_HOUR = 20;
+    int EMP_FULLTIME_WORK_HOUR = 8;
+    int EMP_PARTTIME_WORK_HOUR = 4;
+    int EMP_WORK_DAYS = 20;
+    int EMP_WORK_HOUR = 100;
+
+    public int calculateEmployeeWage(){
+
+        int emp_wage = 0;
         int total_wage = 0;
-        double daily_emp_wage =0;
         int total_emp_work_hour = 0;
         int total_emp_work_day = 0;
 
+        while(total_emp_work_hour<=EMP_WORK_HOUR && total_emp_work_day<EMP_WORK_DAYS){
 
-        while(total_emp_work_hour<= EMP_WORK_HOUR && total_emp_work_day < EMP_WORK_DAYS ){
-            total_emp_work_day++;
-            int empcheck = (int) Math.floor(Math.random() * 10 % 3);
+            total_emp_work_day ++;
+            int emp_check = (int)Math.floor(Math.random()*10%3);
 
-            if (empcheck == FULL_TIME_EMP) {
-                System.out.println("Employee are present for full time.");
-                daily_emp_wage = EMP_FULL_TIME_HOUR * EMP_RATE_PER_HOUR;
-                total_emp_work_hour += EMP_FULL_TIME_HOUR;
+            if(emp_check == FULL_TIME_EMP){
+                emp_wage = EMP_RATE_PER_HOUR * EMP_FULLTIME_WORK_HOUR;
+                total_emp_work_hour += EMP_FULLTIME_WORK_HOUR;
             }
-            else if (empcheck == PART_TIME_EMP) {
-                System.out.println("Employee are present for part time.");
-                daily_emp_wage = EMP_PART_TIME_HOUR * EMP_RATE_PER_HOUR;
-                total_emp_work_hour += EMP_PART_TIME_HOUR;
+            else if(emp_check == PART_TIME_EMP){
+                emp_wage = EMP_RATE_PER_HOUR * EMP_PARTTIME_WORK_HOUR;
+                total_emp_work_hour += EMP_PARTTIME_WORK_HOUR;
             }
-            else {
-                System.out.println("Employee are not present");
-                daily_emp_wage = 0;
+            else{
+                emp_wage = 0;
                 total_emp_work_hour += 0;
             }
-            total_wage += daily_emp_wage;
-
-            System.out.println("Employee Daily Wage is : " + daily_emp_wage);
+            total_wage += emp_wage;
         }
-        System.out.println("The Wages for a month is :" +total_wage);
-        System.out.println("Total wages of working hours 100 and max days 20 is for month : "+total_wage);
+        System.out.println("Total wages of still condition is : ");
+        return total_wage;
+    }
+
+    public static void main(String[] args) {
+
+        Employee_wage emp = new Employee_wage();
+        System.out.println(emp.calculateEmployeeWage());
     }
 }
